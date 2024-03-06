5addc02fd696c2721e8a5a2519f215e40f547b87
James <jamesrwyatt35@gmail.com>
2024-03-05 22:23:31 -0500

    Solution looked up, working test case

--- src/main/java/org/jameskodes/LinkedListCycle.java
+++ src/main/java/org/jameskodes/LinkedListCycle.java
@@ -11,20 +11,43 @@ public class LinkedListCycle {
 
     public boolean hasCycle(ListNode head) {
 
-        // See if there is a head and a next.
+        ListNode slow = head, fast =head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // My attempt at solution failed on Leet Code passed on my machine ...
+    public boolean hasCycleOne(ListNode head) {
+
+        // See if there is a head and a next.`
         if(head == null || head.next == null) { return false;}
 
         // Create collection for node values
         HashMap<Integer, Integer> valueHolder = new HashMap<>();
         // Add first Value
-        valueHolder.put(head.val, head.val);
+        valueHolder.put(head.val, head.next.val);
 
         while(head.next != null) { // Loop while until there is no next
 
-            if(valueHolder.containsKey(head.next.val)) { // If next value is found in "valueHolder" means there is a cycle
-                return true;
+            if(valueHolder.containsKey(head.next.val)) {
+                // If next value is found in "valueHolder" means there is a cycle
+                // Now check if current next values are the same
+                if(valueHolder.get(head.next.val) == head.next.val) {
+                    // if values are equal, proper loop
+                    return true;
+                } else {
+                    // else duplicate heads
+                    return false;
+                }
             } else { // if not found, add and continue to next
-                valueHolder.put(head.val, head.val);
+                valueHolder.put(head.val, head.next.val);
                 head = head.next;
             }
         }
--- src/test/java/LinkedListCycleTest.java
+++ src/test/java/LinkedListCycleTest.java
@@ -40,6 +40,28 @@ public class LinkedListCycleTest {
     public void testThree() {
         boolean answer = linkedListCycle.hasCycle(null);
         Assert.assertFalse("Returned wrong: " + answer,answer);
+    }
+
+    @Test
+    // Input: head = [-21,10,17,8,4,26,5,35,33,-7,-16,27,-12,6,29,-12,5,9,20,14,14,2,13,-24,21,23,-21,5]
+    //        Output: false
+    //        Explanation: -21 does not loop properly
+    // Test case will not work becua
+    public void testTwentyTwo() {
+        ListNode nodeThree = new ListNode(3);
+        ListNode nodeTwo = new ListNode(2);
+        ListNode nodeThreeFake = new ListNode(3);
+        ListNode nodeNegFour = new ListNode(-4);
+
+
+        nodeTwo.next = nodeThreeFake;
+        nodeThreeFake.next = nodeNegFour;
+        nodeNegFour.next = null;
+        nodeThree.next = nodeTwo;
+
+        boolean answer = linkedListCycle.hasCycle(nodeThree);
+
+        Assert.assertFalse("Returned wrong: " + answer,answer);
 
     }
 }
