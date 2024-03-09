e8eaea5475d0d9333f39086405d0e7efd74b8b14
James <jamesrwyatt35@gmail.com>
2024-03-09 17:20:43 -0500

    Outline for Daily leet code MinimumCommonValue

---
+++ src/main/java/org/jameskodes/MinimumCommonValue.java
@@ -0,0 +1,13 @@
+package org.jameskodes;
+
+/*
+*   2540. Minimum Common Value
+* Leet Code Daily 03/09/2024
+ */
+public class MinimumCommonValue {
+
+    public int getCommon(int[] nums1, int[] nums2) {
+
+        return -1;
+    }
+}
