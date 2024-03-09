40778031ec9e7e1912463d0188ee248ae1c2fc94
James <jamesrwyatt35@gmail.com>
2024-03-09 17:27:02 -0500

    MinimumCommonValue - test case working

--- src/test/java/MinimumCommonValueTest.java
+++ src/test/java/MinimumCommonValueTest.java
@@ -1,2 +1,28 @@
+import org.jameskodes.MinimumCommonValue;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MinimumCommonValueTest {
+
+    MinimumCommonValue minimumCommonValue;
+
+    @Before
+    public void init() {
+        minimumCommonValue = new MinimumCommonValue();
+    }
+
+    @Test
+    public void testCaseOne() {
+        int answer = minimumCommonValue.getCommon( new int[]{1,2,3,4}, new int[] {2,3,4,5});
+
+        Assert.assertEquals(2, answer);
+    }
+
+    @Test
+    public void testCaseTwo() {
+        int answer = minimumCommonValue.getCommon( new int[]{1,2,3,6}, new int[] {2,3,4,5});
+
+        Assert.assertEquals(2, answer);
+    }
 }
